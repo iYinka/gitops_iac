d4edca0cc58762164fa2767f8f83bdecd230f356
iYinka <olayinkav28@gmail.com>
2024-03-02 10:40:23 +0100

    terraform bucket name updated

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "gitops-bkt"
-    key    = "terra_state_file/terraform.tfstate"
+    key    = "terra_state_file/terraform.state"
     region = "us-east-1"
   }
 }
